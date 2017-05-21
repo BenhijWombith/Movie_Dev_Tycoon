2a462b652766092f74f45fd02c8441a928abffb5
BenhijWombith <culhanethomas6@gmail.com>
2017-05-20 20:49:18 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+var Movie_Dev_Tycoon_BenhijWombithProductions   = { modPath: '', data: {} };
+
+(function(){
+	Movie_Dev_Tycoon_BenhijWombithProductions.modPath = GDT.getRelativePath();
+	var ready = function () {};
+	var error = function () {};
+	GDT.loadJs(['main/code_before.js', 'main/code.js', 'main/code_after.js'], ready, error);
+})();
